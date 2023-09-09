c12ef84a85ab3fdd405f7b3497c5ab4635a9deb4
qusaiisam <qusaiisam@gmail.com>
2023-09-09 12:41:52 -0700

    C - Variables, if, else, while

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,14 +12,14 @@ int main(void)
 	for (i = 0; i <= 98; i++)
 	{
 
-		for (j = 0; j <= 99; j++)
+		for (j = i + 1; j <= 99; j++)
 		{
 			putchar((i / 10) + '0');
 			putchar((i % 10) + '0');
 			putchar(' ');
 			putchar((j / 10) + '0');
 			putchar((j % 10) + '0');
-			if (i == 98 && (j += 99))
+			if (i == 98 && (j == 99))
 			continue;
 			putchar(',');
 			putchar(' ');

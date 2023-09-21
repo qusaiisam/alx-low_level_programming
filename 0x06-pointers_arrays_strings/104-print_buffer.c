22b60c5b29c01729c11b3327b302cfd9ab7e33cb
qusaiisam <qusaiisam@gmail.com>
2023-09-21 15:15:29 -0700

    Function that prints a buffer.

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -34,5 +34,17 @@ void print_buffer(char *b, int size)
 				printf(" ");
 			}
 		}
+		for (i = 0; i < j; i++)
+		{
+			int c = *(b + o + i);
+
+			if (c < 32 || c > 132)
+			{
+				c = '.';
+			}
+			printf("%c", c);
+		}
+		printf("\n");
+		o += 10;
 	}
 }

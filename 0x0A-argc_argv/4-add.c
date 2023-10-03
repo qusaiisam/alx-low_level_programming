3275099047da942c9dcdc2b14967a9a381e7644b
qusaiisam <qusaiisam@gmail.com>
2023-10-02 17:51:29 -0700

    Program that adds positive numbers.

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - adds positive numbers.
+* _atoi - converts a string to an integer
+* @argc: number of arguments
+* @argv: array of arguments
+*
+* Return: Always 0 (Success)
+*/
+
+int main(int argc, char *argv[])
+{
+	int i, sum = 0;
+
+	if (argc > 1)
+	{
+		for (i = 0; i < argc; i++)
+		{
+			sum = sum + _atoi(argv[i]);
+		}
+		printf("%d", sum);
+	}
+	return (0);
+}

fd5e1cb1fd797244be1f45a1b0663357dde1815d
qusaiisam <qusaiisam@gmail.com>
2023-10-02 19:25:29 -0700

    Program that multiplies two numbers.

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -26,22 +26,25 @@ int _atoi(char *s)
 	{
 		if (s[i] == '-')
 			++d;
+
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			digit = s[i] - '0';
+
 			if (d % 2)
 				digit = -digit;
-				n = n * 10 + digit;
-				f = 1;
+			n = n * 10 + digit;
+			f = 1;
 
-				if (s[i + 1] < '0' || s[i + 1] > '9')
-					break;
-				f = 0;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
 		}
 		i++;
 	}
 	if (f == 0)
 		return (0);
+
 	return (n);
 }
 
@@ -62,9 +65,11 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
+
 	num1 = _atoi(argv[1]);
 	num2 = _atoi(argv[2]);
 	result = num1 * num2;
 	printf("%d\n", result);
 	return (0);
+
 }

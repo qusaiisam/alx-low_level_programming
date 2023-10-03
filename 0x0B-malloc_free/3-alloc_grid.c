84e6d86ca7e2d320efd6b5fbfa5b3da8b8b086f0
qusaiisam <qusaiisam@gmail.com>
2023-10-03 15:48:14 -0700

    Function that concatenates two stringgs.

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* alloc_grid - nested loop to make grid
+* @width: width input
+* @height: height input
+* Return: pointer to 2 dim. array
+*/
+
+int **alloc_grid(int width, int height)
+{
+	int **n;
+	int x, y;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	n = malloc(sizeof(int *) * height);
+
+	if (mee == NULL)
+		return (NULL);
+
+	for (x = 0; x < height; x++)
+	{
+		n[x] = malloc(sizeof(int) * width);
+
+		if (n[x] == NULL)
+		{
+			for (; x >= 0; x--)
+				free(n[x]);
+			free(mee);
+			return (NULL);
+		}
+	}
+
+	for (x = 0; x < height; x++)
+	{
+		for (y = 0; y < width; y++)
+			n[x][y] = 0;
+	}
+	return (n);
+}

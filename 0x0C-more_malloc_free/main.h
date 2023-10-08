c3bb9a606e314e4a269570fe4ddeaf14634c6f4c
qusaiisam <qusaiisam@gmail.com>
2023-10-07 19:49:24 -0700

    Creat main.h & pitchar files

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
@@ -8,4 +11,5 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-	#endif
+
+#endif
